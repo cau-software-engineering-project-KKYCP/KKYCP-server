7016bf8bc7b196aade66f097d6462478020452ee
Dong-Yun Park <ehddbs7458@gmail.com>
2024-05-10 01:05:07 +0900

    Initial commit

---
+++ src/main/java/org/kkycp/server/KkycpServerApplication.java
@@ -0,0 +1,13 @@
+package org.kkycp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkycpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkycpServerApplication.class, args);
+	}
+
+}
