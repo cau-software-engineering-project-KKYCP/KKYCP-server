1af2f7eb1f55c19c6410055cbe0356aa6a68a57c
foopky <110583926+foopky@users.noreply.github.com>
2024-05-24 00:54:37 +0900

    Update ProjectService.java

--- src/main/java/org/kkycp/server/services/ProjectService.java
+++ src/main/java/org/kkycp/server/services/ProjectService.java
@@ -1,14 +1,15 @@
 package org.kkycp.server.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import lombok.RequiredArgsConstructor;
+import org.kkycp.server.domain.Filter;
 import org.kkycp.server.domain.Issue;
 import org.kkycp.server.domain.Project;
 import org.kkycp.server.repo.ProjectRepo;
-import org.kkycp.server.repo.issue.IssueRepo;
-import org.kkycp.server.repo.issue.IssueSearchCondition;
+import org.kkycp.server.repo.issue.CountIssueRepo;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -20,8 +21,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequiredArgsConstructor
 public class ProjectService {
     private final ProjectRepo projectRepo;
-
-    private final IssueRepo issueRepo;
 
     public long createProject(String projectName) {
         Project project = new Project(projectName);
@@ -49,9 +48,20 @@ public class ProjectService {
         }
     }
 
-    public List<Issue> findIssues(long projectId, IssueSearchCondition condition, int page, int quantity){
-        int offset = (page - 1) * quantity;
-        return issueRepo.search(projectId, condition, offset, quantity);
+    public List<Issue> ShowReportStatistics(long projectid, Filter filter){
+        Project project = findProject(projectid);
+        CountIssueRepo countIssueRepo;
+        List<Issue> issueList = new ArrayList<>();
+        if (filter.filtercase==1){
+            issueList = countIssueRepo.countIssuebyDate(project, filter.from, filter.to);
+        }
+        else if(filter.filtercase==2){
+            issueList = countIssueRepo.countIssuebyPriority(project ,filter.priority);
+        }
+        else if(filter.filtercase==3){
+            issueList = countIssueRepo.countIssuebyStatus(project, filter.status);
+        }
+
+        return issueList;
     }
 }
-
