add7378aa07993c2214bc930a93ec0a8bae7ff61
foopky <110583926+foopky@users.noreply.github.com>
2024-05-24 01:27:05 +0900

    Update CommentRepo.java

--- src/main/java/org/kkycp/server/repo/CommentRepo.java
+++ src/main/java/org/kkycp/server/repo/CommentRepo.java
@@ -1,9 +1,10 @@
 package org.kkycp.server.repo;
 
 import org.kkycp.server.domain.Issue;
+import org.kkycp.server.domain.Comment;
 
 public interface CommentRepo {
-    void StoreComment(Issue issue, long commentid, String comments);
+    void StoreComment(Issue issue, Comment comment);
     void DeleteComment(Issue issue, long commentid);
-    void ModificateComment(Issue issue, long commentid, String comments);
+    void ModificateComment(Issue issue, long commentid, String content);
 }
