e3b0e086a1664307ab2319159e009fed307bde60
박동윤 <ehddbs7458@gmail.com>
2024-05-24 18:56:06 +0900

    feat: user authorization check

--- src/main/java/org/kkycp/server/exceptions/UserNotParticipatingException.java
+++ src/main/java/org/kkycp/server/exceptions/UserNotParticipatingException.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 public class UserNotParticipatingException extends RuntimeException {
+    public UserNotParticipatingException() {
+        super("User is not participating the project");
+    }
+
     public UserNotParticipatingException(String message) {
         super(message);
     }
