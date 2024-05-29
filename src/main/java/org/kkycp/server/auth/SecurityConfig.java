e29f705bf064dee4fbebeb8d1eaa493df35bb704
박동윤 <ehddbs7458@gmail.com>
2024-05-29 20:15:14 +0900

    Hotfix: logout returns 404 problem

--- src/main/java/org/kkycp/server/auth/SecurityConfig.java
+++ src/main/java/org/kkycp/server/auth/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -41,7 +42,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         request -> request
                                 .requestMatchers("/", "/index.html", "/signup", "/login").permitAll()
-                                .anyRequest().authenticated());
+                                .anyRequest().authenticated())
+                .logout(logout -> logout.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler()));
     }
 
     public static CorsConfigurationSource corsSource() {
